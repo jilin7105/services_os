ea99a379d9b7da1341a78af8bf08dc9c7184cdaa
jilin7105 <443500251@qq.com>
2019-08-14 17:14:20 +0800

    no message

--- weida/app/controllers/dev/RouterController.php
+++ weida/app/controllers/dev/RouterController.php
@@ -11,10 +11,19 @@ class RouterController extends ControllerBase
     }
 
     public function listAction(){
-       $router = Router::find();
+       $router = Router::find(["status = :status:",'bind'=>['status'=>1]]);
        $this->view->setVar("routers",$router);
+       
     }
 
+    public function deleteAction(){
+    	$id = $this->request->get('id'); 
+    	$router = Router::findFrist($id);
+    	$router->status = 0;
+    	$router->save();
+    	$this->response->redirect('/router-list');
+
+    }
     public function addAction()
     {
 
