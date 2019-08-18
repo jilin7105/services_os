523be3902a62efda6f6a6c09333ee5b96efd272d
yyqj <443500251@qq.com>
2019-08-18 08:57:31 +0800

    1

--- weida/app/controllers/dev/ControllerBase.php
+++ weida/app/controllers/dev/ControllerBase.php
@@ -6,9 +6,13 @@ use Phalcon\Http\Response;
 class ControllerBase extends Controller
 {
 	public $response;
+	public $use_common_template = true;
 	public function initialize()
     {
-    	$this->view->setTemplateAfter('common');
+    	if($use_common_template){
+    		$this->view->setTemplateAfter('common');
+    	}
+    	
         $this->response = new Response();
     }
 
